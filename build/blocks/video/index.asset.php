024c2b87c7d498ef48b2e599ffe31fa236072475
Abu Foysal <abu.foysal@sourcetop.com>
2025-07-08 21:41:57 +0600

    SCSS code optimization

--- build/blocks/video/index.asset.php
+++ build/blocks/video/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '9a55942ab1296c44a315');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'aebb7c3b9121aa834263');
